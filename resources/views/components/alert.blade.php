53ddf76dbe74f466782adcf671ff0d46e4760801
EstivenJQM <juan_quiroz82202@elpoli.edu.co>
2026-04-15 12:54:20 -0500

    style (el mensaje de confirmacion de accion desaparece solo al paso de 4 segundos)

--- resources/views/components/alert.blade.php
+++ resources/views/components/alert.blade.php
@@ -10,7 +10,16 @@
     $icon = $icons[$type] ?? 'bi-info-circle-fill';
 @endphp
 
-<div class="alert alert-{{ $type }} alert-dismissible fade show" role="alert">
+<div class="alert alert-{{ $type }} alert-dismissible fade show auto-dismiss" role="alert">
     <i class="bi {{ $icon }} me-1"></i> {{ $message }}
     <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
 </div>
+
+<script>
+    document.querySelectorAll('.auto-dismiss').forEach(function (el) {
+        setTimeout(function () {
+            var bsAlert = bootstrap.Alert.getOrCreateInstance(el);
+            bsAlert.close();
+        }, 4000);
+    });
+</script>
